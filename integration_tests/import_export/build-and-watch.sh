ba153000e70e625c738ae80dcc229a47cc4511c7
Shanqing Cai <cais@google.com>
2018-05-06 11:51:43 -0400

    Demo for tf.io.*: import_export

---
+++ integration_tests/import_export/build-and-watch.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Copyright 2018 Google LLC
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+# =============================================================================
+
+# Builds the benchmarks demo for TensorFlow.js Layers.
+# Usage example: do this from the 'benchmarks' directory:
+#   ./build-benchmarks.sh
+#
+# Then open the demo HTML page in your browser, e.g.,
+#   http://localhost:8000/dist
+
+set -e
+
+DEMO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+DEMO_PORT=8000
+while true; do
+  if [[ "$1" == "--port" ]]; then
+    DEMO_PORT=$2
+    shift 2
+  elif [[ -z "$1" ]]; then
+    break
+  else
+    echo "ERROR: Unrecognized argument: $1"
+    exit 1
+  fi
+done
+
+DATA_ROOT="${DEMO_DIR}/dist/data"
+
+echo Building local TensorFlow.js Layers NPM package...
+cd ../..
+yarn build
+yarn link
+
+cd ${DEMO_DIR}
+yarn
+yarn link "@tensorflow/tfjs-layers"
+yarn build
+
+echo
+echo "-----------------------------------------------------------"
+echo "Once the HTTP server has started, you can view the demo at:"
+echo "  http://localhost:${DEMO_PORT}/dist"
+echo "-----------------------------------------------------------"
+echo
+
+node_modules/http-server/bin/http-server -p "${DEMO_PORT}"
